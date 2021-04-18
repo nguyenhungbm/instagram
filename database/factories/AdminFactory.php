84b7b409e7eef6407eab2454e5a1ea0dc42aaf8d
Nguyen <hung0913003358@gmail.com>
2021-04-18 22:50:56 +0700

    Grs

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Factories;
-
+use App\Models\Admin;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\Admin::factory(100)->create();
+        \App\Models\Admin::factory(100)->create();
         \App\Models\User::factory(100)->create();
         \App\Models\Post::factory(100)->create();
     }
