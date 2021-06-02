c2dead365ea63997431397d2149cbf5357644d1e
guestuserss <shopsoda1pk@gmail.com>
2021-06-02 22:31:48 +0700

    G

--- resources/views/layout/comment.blade.php
+++ resources/views/layout/comment.blade.php
@@ -2,23 +2,27 @@
    @csrf
    <textarea autocomplete="off"  class="textarea-{{$value}} textarea-comment{{$value}} textarea-comment" placeholder="{{ __('translate.Add a comment')}}..."></textarea>
    <input type="hidden" value="{{$value}}" class="post-comment{{$value}}">   
-   <button type="button" class="submit-{{$value}}  button-comment disabled position-relative"><div class="position-absolute">{{ __('translate.Post')}}</div>
+   <button type="button" id="submit-{{$value}}" class="  button-comment disabled position-relative"><div class="position-absolute">{{ __('translate.Post')}}</div>
    <img src="{{ asset('img/loading.gif')}}" class="w-30 load-comment" style="display:none;">
    </button>
 </form>
 <script>
    //không cho người dùng đăng khi chưa comment
    $(function(){  
-   $('.textarea-{{$value}}').on('keyup',function(){
+   $('.textarea-{{$value}}').on('keyup',function(event){
       if(!$('.textarea-{{$value}}').val())
-      $('.submit-{{$value}}').addClass('disabled'); 
+      $('#submit-{{$value}}').addClass('disabled'); 
       else{ 
-      $('.submit-{{$value}}').removeClass('disabled');
+      $('#submit-{{$value}}').removeClass('disabled');
+      if(event.keyCode === 13) {
+         event.preventDefault();
+         document.getElementById("submit-"+{{$value}}).click();
+       }
       }
    })
    
    //comment
-   $(".submit-{{$value}}").on('click',function(e){
+   $("#submit-{{$value}}").on('click',function(e){
    e.preventDefault();
    var URL= $(this).parents('form').attr('action');
    var c_comment =$('.textarea-comment{{$value}}').val();
